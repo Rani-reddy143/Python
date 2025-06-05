09a202c3682cbf00c97d8a8d23d4e798c36ff102
rani reddy <jaanureddy647@gmail.com>
2025-06-05 19:37:01 +0530

    nested if_else

---
+++ nested_ifelse.py
@@ -0,0 +1,9 @@
+age=21
+if age>=18:
+    print("eligible to vote")
+    if age>20:
+        print("eligible to get marry")
+    else:
+        print("not eligible")
+else:
+    print("not eligible to vote")
