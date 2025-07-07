2b44d14b1baf9f4c8835c8668ad4897abe278830
rani reddy <jaanureddy647@gmail.com>
2025-07-07 20:54:09 +0530

    checking the given string/number is polindrome or not

---
+++ polindrome.py
@@ -0,0 +1,15 @@
+my_str="mam"
+reversed_str=reversed(my_str)
+if list(my_str)==list(reversed_str):
+    print("Polindrome string")
+else:
+    print("Not a polindrome")
+    
+    
+'''
+text=input("enter the string")
+if text==text[::-1]:
+    print(f"{text} is a polindrome.")
+else:
+    print(f"{text} is not a polindrome")
+'''
